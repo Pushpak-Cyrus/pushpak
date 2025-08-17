93b7067caa2fabf356a7a7659ff9cb74a626a505
Pushpak <kumarpushpak1309@gmail.com>
2025-08-17 12:24:28 +0530

    Fix syntax error in area_circle function call

--- circumference.py
+++ circumference.py
@@ -1,7 +1,6 @@
 import math
 
 def main():
-
     print("What would you like to calculate?")
     print("1.circumfernce")
     print("2.diameter")
@@ -9,32 +8,30 @@ def main():
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
-     circumference = circle_circumference()
-
+        circumference = circle_circumference()
     elif choice == 2:
-     diameter = diameter_of_circle()
+        diameter = diameter_of_circle()
     elif choice == 3:
-     area = area_circle()
-
+        area = area_circle()
     else:
         print("Invalid choice")
 
-def diameter_of_circle ():
-        r = float(input("Enter radius of the circle: "))
-        diameter = r * 2
-        print(f"Your diameter is: {diameter:.2f}")
-        return diameter
+def diameter_of_circle():
+    r = float(input("Enter radius of the circle: "))
+    diameter = r * 2
+    print(f"Your diameter is: {diameter:.2f}")
+    return diameter
 
 def circle_circumference():
-       r = float(input("Enter the radius of the circle: "))
-       circumference = 2 * math.pi * r
-       print(circumference)
-       return circumference
+    r = float(input("Enter the radius of the circle: "))
+    circumference = 2 * math.pi * r
+    print(circumference)
+    return circumference
 
-def area_circle ():
-      r = float(input("Enter the radius of the circle: "))
-      area = math.pi * r ** 2
-      print(f"The area of the circle is: {area:.2f}")
-      return area
+def area_circle():
+    r = float(input("Enter the radius of the circle: "))
+    area = math.pi * r ** 2
+    print(f"The area of the circle is: {area:.2f}")
+    return area
 
 main()
